732f44841498563c4706fdaa5251d773411bd49a
iumehara <iumehara10@gmail.com>
2021-10-12 18:22:11 -0400

    clean up e2e tests

--- cistatusbarE2ETests/happyPathTests.swift
+++ cistatusbarE2ETests/happyPathTests.swift
@@ -1,7 +1,8 @@
 import XCTest
 
 class happyPathTests: XCTestCase {
-     var app: XCUIApplication!
+    var app: XCUIApplication!
+    let timeout: TimeInterval = 10
     
     override func setUp() {
         super.setUp()
@@ -11,80 +12,81 @@ class happyPathTests: XCTestCase {
     func testHappyPath() throws {
         print("launching app")
         app.launch()
-        wait()
         
         print("saving new job")
-        clickMenuButton()
+        clickMenuButtonWithNoJobs()
         clickPreferencesMenuItem()
         clickAddJobButton()
         saveNewJob()
-
+        
         print("checking run status for new job")
-        clickMenuButton()
-        wait()
+        clickMenuButtonWithSuccessfulJob()
         assertJobStatus()
-
+        
         print("deleting new job")
         clickPreferencesMenuItem()
         selectNewJobInJobList()
         clickDeleteJobButton()
     }
-
-    private func wait() {
+    
+    private func clickMenuButtonWithNoJobs() {
         sleep(5)
+        let menuButton = app.buttons["MenuIcon"]
+        XCTAssertTrue(menuButton.waitForExistence(timeout: timeout))
+        menuButton.click()
     }
     
-    private func clickMenuButton() {
-        let menuButton = app.buttons["MenuIcon"]
-        XCTAssertTrue(menuButton.exists)
+    private func clickMenuButtonWithSuccessfulJob() {
+        let menuButton = app.buttons["🟢"]
+        XCTAssertTrue(menuButton.waitForExistence(timeout: timeout))
         menuButton.click()
     }
     
     private func clickPreferencesMenuItem() {
         let preferencesMenuItem = app.menuItems["Preferences..."]
-        XCTAssertTrue(preferencesMenuItem.exists)
+        XCTAssertTrue(preferencesMenuItem.waitForExistence(timeout: timeout))
         preferencesMenuItem.click()
     }
     
     private func clickAddJobButton() {
         let addJobButton = app.buttons["add job"]
-        XCTAssertTrue(addJobButton.exists)
+        XCTAssertTrue(addJobButton.waitForExistence(timeout: timeout))
         addJobButton.click()
     }
     
     private func saveNewJob() {
         let nameTextField = app.textFields["name"]
-        XCTAssertTrue(nameTextField.exists)
+        XCTAssertTrue(nameTextField.waitForExistence(timeout: timeout))
         nameTextField.doubleClick()
         nameTextField.typeKey(XCUIKeyboardKey.delete, modifierFlags: [])
         nameTextField.doubleClick()
         nameTextField.typeKey(XCUIKeyboardKey.delete, modifierFlags: [])
         nameTextField.typeText("Starter-Workflows CI")
-
+        
         let urlTextField = app.textFields["url"]
-        XCTAssertTrue(urlTextField.exists)
+        XCTAssertTrue(urlTextField.waitForExistence(timeout: timeout))
         urlTextField.click()
         urlTextField.typeText("https://api.github.com/repos/actions/starter-workflows/actions/workflows/1678965/runs")
-
+        
         let saveButton = app.buttons["save"]
-        XCTAssertTrue(saveButton.exists)
+        XCTAssertTrue(saveButton.waitForExistence(timeout: timeout))
         saveButton.click()
     }
     
     private func assertJobStatus() {
         let successfulJob = app.menuItems["🟢 Starter-Workflows CI"]
-        XCTAssertTrue(successfulJob.exists)
+        XCTAssertTrue(successfulJob.waitForExistence(timeout: timeout))
     }
-        
+    
     private func selectNewJobInJobList() {
         let jobListItem = app.staticTexts["Starter-Workflows CI"]
-        XCTAssertTrue(jobListItem.exists)
+        XCTAssertTrue(jobListItem.waitForExistence(timeout: timeout))
         jobListItem.click()
     }
-
+    
     private func clickDeleteJobButton() {
         let deleteJobButton = app.buttons["remove job"]
-        XCTAssertTrue(deleteJobButton.exists)
+        XCTAssertTrue(deleteJobButton.waitForExistence(timeout: timeout))
         deleteJobButton.click()
         let jobListItem = app.staticTexts["Starter-Workflows CI"]
         XCTAssertFalse(jobListItem.exists)
